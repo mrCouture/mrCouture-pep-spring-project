b48b04401f7463a6eef71db9a8b65a043ec558fd
mrCouture <renecouture26@yahoo.com>
2023-12-02 13:36:36 -0500

    Post a new message endpoint is setup. tests pass.

--- src/main/java/com/example/repository/AccountRepository.java
+++ src/main/java/com/example/repository/AccountRepository.java
@@ -1,6 +1,7 @@
 package com.example.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.entity.Account;
@@ -12,7 +13,10 @@ public interface AccountRepository extends JpaRepository<Account,Integer>
 
 	Account findAccountByUsernameAndPassword(String username,String password);
 
-	
+	//Can't rename all the variables to camelCase because 
+	//tests would need to change and I can't change those
+	@Query("from Account where account_id=?1")
+	Account findAccountByAccountId(Integer accountId);
 
 	//Error: No property accountId found for type Account! Did you mean 'account_id'?
 	//Account findAccountByAccountId(Integer account_id);
